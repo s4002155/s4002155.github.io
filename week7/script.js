fae1844672f824d8ccdf5e2b7a63f56087f71a16
s4002155 <s4002155@student.rmit.edu.au>
2024-05-09 12:29:00 +1000

    week7

---
+++ week7/script.js
@@ -0,0 +1,33 @@
+const airportAudio = document.querySelector("#airport-audio");
+console.log(airportAudio);
+// airportAudio.removeAttribute("controls");
+const playButton = document.querySelector("#play-button");
+console.log(playButton);
+
+playButton.addEventListener("click", playAirportAudio);
+
+function playAirportAudio() {
+    airportAudio.play();
+}
+
+const pauseButton = document.querySelector("#pause-button");
+console.log(pauseButton);
+
+pauseButton.addEventListener("click", pauseAirportAudio);
+
+function pauseAirportAudio() {
+    airportAudio.pause();
+}
+
+const popButton = document.querySelector("#pop-button");
+console.log(popButton);
+
+const popSound = document.querySelector("#pop-sound")
+console.log(popSound);
+
+popButton.addEventListener("click", popIt);
+
+function popIt() {
+     console.log("did you just click me?");
+    popSound.play();
+}
